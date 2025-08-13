dea74e1d4c649f24189db0f690ef79e659b58fa2
JawadYT804 <jadixop@gmail.com>
2025-08-13 07:14:33 +0500

    Create herokuApiSelector.js

---
+++ herokuApiSelector.js
@@ -0,0 +1,11 @@
+const herokuKeys = [
+  "HRKU-AAKPmp63-EAct3qWY4Hwz4QH3SjuKWQOawLsQbCCGoDA_____w2JBt9xtt8V",
+  "HRKU-AAiM2iM1fj51HV_suyoRmgZNL77YyDSgefWKJUXgCPuw_____wHp6U4dbjAs",
+  "HRKU-AAKPmp63-EAct3qWY4Hwz4QH3SjuKWQOawLsQbCCGoDA_____w2JBt9xtt8V"
+];
+
+function getRandomHerokuKey() {
+  return herokuKeys[Math.floor(Math.random() * herokuKeys.length)];
+}
+
+module.exports = { getRandomHerokuKey };
