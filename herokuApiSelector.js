3553d953c69699ef52ca205c9009f42f7b2dda35
JawadYT804 <jadixop@gmail.com>
2025-08-13 20:19:49 +0500

    Update herokuApiSelector.js

--- herokuApiSelector.js
+++ herokuApiSelector.js
@@ -1,7 +1,7 @@
 const herokuKeys = [
-  "HRKU-AAKPmp63-EAct3qWY4Hwz4QH3SjuKWQOawLsQbCCGoDA_____w2JBt9xtt8V",
-  "HRKU-AAiM2iM1fj51HV_suyoRmgZNL77YyDSgefWKJUXgCPuw_____wHp6U4dbjAs",
-  "HRKU-AAKPmp63-EAct3qWY4Hwz4QH3SjuKWQOawLsQbCCGoDA_____w2JBt9xtt8V"
+  "HRKU-AAuwoUxCk-uxAbO4n-XbEeshD_NVdtSDObqU1SVqbtoA_____wK2CHAxpWnu",
+  "HRKU-AAuwoUxCk-uxAbO4n-XbEeshD_NVdtSDObqU1SVqbtoA_____wK2CHAxpWnu",
+  "HRKU-AAuwoUxCk-uxAbO4n-XbEeshD_NVdtSDObqU1SVqbtoA_____wK2CHAxpWnu"
 ];
 
 function getRandomHerokuKey() {
